67c67ce421d17134d30cfe1f0e6fcccd0baa185c
Elie Guilbaud <elie.guilbaud@sfr.fr>
2020-11-03 23:36:50 +0100

    scene menu et option

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void loadlevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
