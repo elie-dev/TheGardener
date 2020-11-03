4634e6cc68f8e4a8d6c59b69f1116bfddb7d8078
Elie Guilbaud <elie.guilbaud@sfr.fr>
2020-11-03 21:58:44 +0100

    first commit

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movement : MonoBehaviour
+{
+
+    private Rigidbody2D rbPlayer;
+
+    private Vector2 moveInput;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbPlayer.velocity = moveInput * speed;
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
