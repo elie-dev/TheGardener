6543c78d3b88d31aa39357deb04a29771d8a19ba
Elie Guilbaud <elie.guilbaud@sfr.fr>
2020-11-04 11:06:16 +0100

    scene menu et option

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -15,14 +15,51 @@ public class movement : MonoBehaviour
     void Start()
     {
         rbPlayer = this.GetComponent<Rigidbody2D>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        // déplacement
         rbPlayer.velocity = moveInput * speed;
+
+
+
+        //verifie l'orientation
+        if(moveInput.x == 0 & moveInput.y == 0)
+        {
+            Debug.Log("immobile");
+        } else if (Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
+        {
+            Vector3 rotationVector = transform.rotation.eulerAngles;
+            if (moveInput.x < 0)
+            {
+                rotationVector.y = 180;
+                Debug.Log("gauche");
+            } else
+            {
+                rotationVector.y = 0;
+                Debug.Log("droite");
+            }
+
+            transform.rotation = Quaternion.Euler(rotationVector);
+        } else
+        {
+            if (moveInput.y > 0)
+            {
+                Debug.Log("haut");
+            } else
+            {
+                Debug.Log("bas");
+            }
+        }
     }
 
+    /// Recupère les input sous forme de vecteur (X, Y)
+    /// 
+    /// 
+    /// <param name="value"></param>
     public void OnMovement(InputValue value)
     {
         moveInput = value.Get<Vector2>();
---
+++ Assets/Scripts/units.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class units : MonoBehaviour
+{
+    public string unitName;
+    public string type;
+
+    public float runSpeed;
+    public float walkSpeed;
+
+    private float deathTime;
+    public float aquisitionRange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
