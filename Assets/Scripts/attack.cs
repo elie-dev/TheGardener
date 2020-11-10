ce02eb71ed17deca8c177b20fb77a9a76c44b6c5
Elie Guilbaud <elie.guilbaud@sfr.fr>
2020-11-10 21:47:40 +0100

    attaque et animation walk, sprint, idle

---
+++ Assets/Scripts/attack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    public float angleAttack;
+    public GameObject coneAngle;
+    public GameObject coneAttack;
+
+    public int damage;
+
+    public List<Collider2D> ennemiesHits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnAttack()
+    {
+        Collider2D[] hitEnnemies = coneAttack.GetComponent<attackCollide>().TriggerList.ToArray();
+        foreach (Collider2D ennemy in hitEnnemies)
+        {
+            ennemy.gameObject.GetComponent<units>().takeDamage(damage);
+        }
+    }
+
+    public void changeAngleAttack(int angle)
+    {
+        var rotationVector = coneAngle.transform.rotation.eulerAngles;
+        rotationVector.z = angle;
+        coneAngle.transform.rotation = Quaternion.Euler(rotationVector);
+    }
+}
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,53 +5,139 @@ using UnityEngine.InputSystem;
 
 public class movement : MonoBehaviour
 {
-
     private Rigidbody2D rbPlayer;
+    private Animator anim;
+
+    private attack attackScript;
 
     private Vector2 moveInput;
-    public float speed;
+    private float speed;
+    private bool isSprinting = false;
+    public float walkSpeed;
+    public float sprintSpeed;
+
+    public float slidingSpeed;
+    public float slidingDistance;
+    private float slideSpeed;
+    private Vector3 slideDir;
+
+    private State state;
+    private enum State
+    {
+        Normal,
+        DodgeRollSliding,
+    }
+
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rbPlayer = this.GetComponent<Rigidbody2D>();
-       
+        anim = this.GetComponent<Animator>();
+        attackScript = GetComponent<attack>();
+        speed = walkSpeed;       
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(state);
+        switch(state)
+        {
+            case State.Normal:
+                move();
+                orientation();
+                break;
+            case State.DodgeRollSliding:
+                handleDodgeRollSliding();
+                break;
+        }
+    }
+
+    private bool CanMove(Vector3 dir, float distance)
+    {
+        return Physics2D.Raycast(transform.position, dir, distance).collider == null;
+    }
+
+    public bool TryMove(Vector3 baseMoveDir, float distance)
+    {
+        Vector3 moveDir = baseMoveDir;
+        bool canMove = CanMove(moveDir, distance);
+        if (!canMove)
+        {
+            //cannot move diagonnaly
+            moveDir = new Vector3(baseMoveDir.x, 0f).normalized;
+            canMove = moveDir.x != 0f && CanMove(moveDir, distance);
+            if(!canMove)
+            {
+                //cannot move horizontally
+                moveDir = new Vector3(0f, baseMoveDir.y).normalized;
+                canMove = moveDir.y != 0f && CanMove(moveDir, distance);
+            }
+        }
+
+        if (canMove)
+        {
+            //lastMoveDir = moveDir;
+            transform.position += moveDir * distance;
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    public void move()
+    {
         // déplacement
         rbPlayer.velocity = moveInput * speed;
+    }
 
-
-
+    public void orientation()
+    {
         //verifie l'orientation
-        if(moveInput.x == 0 & moveInput.y == 0)
+        if (moveInput.x == 0 & moveInput.y == 0)
         {
-            Debug.Log("immobile");
-        } else if (Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
+            anim.SetBool("isMoving", false);
+            //Debug.Log("immobile");
+        }
+        else if (Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
         {
-            Vector3 rotationVector = transform.rotation.eulerAngles;
             if (moveInput.x < 0)
             {
-                rotationVector.y = 180;
-                Debug.Log("gauche");
-            } else
+                // gauche
+                anim.SetBool("isMoving", true);
+                anim.SetFloat("Horrizontal", -1);
+                anim.SetFloat("Vertical", 0);
+                attackScript.changeAngleAttack(-90);
+            }
+            else
             {
-                rotationVector.y = 0;
-                Debug.Log("droite");
+                //droite
+                anim.SetBool("isMoving", true);
+                anim.SetFloat("Horrizontal", 1);
+                anim.SetFloat("Vertical", 0);
+                attackScript.changeAngleAttack(90);
             }
 
-            transform.rotation = Quaternion.Euler(rotationVector);
-        } else
+        }
+        else
         {
             if (moveInput.y > 0)
             {
-                Debug.Log("haut");
-            } else
+                //haut
+                anim.SetBool("isMoving", true);
+                anim.SetFloat("Horrizontal", 0);
+                anim.SetFloat("Vertical", 1);
+                attackScript.changeAngleAttack(180);
+            }
+            else
             {
-                Debug.Log("bas");
+                //"bas"
+                anim.SetBool("isMoving", true);
+                anim.SetFloat("Horrizontal", 0);
+                anim.SetFloat("Vertical", -1);
+                attackScript.changeAngleAttack(0);
             }
         }
     }
@@ -63,5 +149,41 @@ public class movement : MonoBehaviour
     public void OnMovement(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnSprint(InputValue value)
+    {
+        if(isSprinting)
+        {
+            isSprinting = false;
+            speed = walkSpeed;
+            anim.SetBool("isSprinting", false);
+        } else
+        {
+            isSprinting = true;
+            speed = sprintSpeed;
+            anim.SetBool("isSprinting", true);
+        }
+    }
+
+    public void OnDash()
+    {
+        if(state == State.Normal && moveInput != new Vector2(0, 0))
+        {
+            state = State.DodgeRollSliding;
+            slideDir = new Vector3(moveInput.x, moveInput.y, transform.position.z);
+            slideSpeed = slidingSpeed;
+        }
+    }
+
+    public void handleDodgeRollSliding()
+    {
+        rbPlayer.velocity = slideDir * slidingSpeed * Time.deltaTime;
+
+        slideSpeed -= slideSpeed * slidingDistance * Time.deltaTime;
+        if (slideSpeed < 5f)
+        {
+            state = State.Normal;
+        }
     }
 }
