a1347630e5841705ecf98d39d0116a847960b1b3
NeaYSi <57349907+NeaYSi@users.noreply.github.com>
2021-02-04 14:14:02 +0100

    controls ps4

---
+++ Assets/LeafControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LeafControls : MonoBehaviour
+{
+    PlayerControls controls;
+    Vector2 move;
+    
+    void awake()
+    {
+        controls = new PlayerControls();
+
+        controls.gamepade.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.gamepade.Move.canceled += ctx => move = Vector2.zero;
+        
+    }
+
+    void Update()
+    {
+        Vector2 m = new Vector2(move.x, move.y) * Time.deltaTime;
+        transform.Translate(m, Space.World);
+
+    }
+}
